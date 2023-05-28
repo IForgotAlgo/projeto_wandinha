1041379801fe97c17395f661782eff40860d42fb
Forgocido <pokenoobgam3r@gmail.com>
2023-05-28 19:57:59 -0300

    Mudei umas Coisinhas

--- projectw.cpp
+++ projectw.cpp
@@ -8,23 +8,14 @@ int main () {
 
     i = 0;
     j = 0;
-
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 6; j++) {
-            cout << "Digite o Valor para " << i << " " << j << ": ";
-            cin >> produtos [i] [j];
-            i++;
-            j++;
-        }
-    }
-}
-    cout << "A Matriz é; " << endl;
     
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 6; j++) {
-            cout << produtos [i] [j] << " ";
+    for (int i = 1; i < 2; i++) {
+        for (int j = 1; j < 2; j++) {
+            cout << "Nome do Produto: " << i << j << "\n Preço: \n Quantidade: ";
+            cin >> produtos [i] [j];
         }
-        cout << endl;
     }
 
-return 0;
+    cout << produtos [1] [1];
+}
+    
